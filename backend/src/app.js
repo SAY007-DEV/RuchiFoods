93d019b70dc562bc848562616283dc7f59efa2e6
SAY007-DEV <mail.to.sayooj.kp@gmail.com>
2026-01-31 11:56:45 +0530

    server added

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,15 @@
-const express = require('express');
+import express from 'express'
+import cors from 'cors'
+
 const app = express();
+
+app.use(cors());
 app.use(express.json());
-app.get('/', (req, res) => res.send('Hello World'));
-app.listen(3000, () => console.log('Server running on port 3000'));
+
+const PORT = 3000;
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port localhost:${PORT}`);
+}
+)
