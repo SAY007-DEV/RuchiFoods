fe5a30e723f70c6c58e07c2c217939fb797df14c
SAY007-DEV <mail.to.sayooj.kp@gmail.com>
2026-02-02 13:06:58 +0530

    bug fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,16 @@
 import express from 'express'
 import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
 
 const PORT = 3000;
 
